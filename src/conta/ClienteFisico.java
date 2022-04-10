84c5d368db0fa147b0e90eead979328f11512418
esterferreirac <63823344+esterferreirac@users.noreply.github.com>
2022-04-10 13:32:50 -0300

    Add files via upload

---
+++ src/conta/ClienteFisico.java
@@ -0,0 +1,13 @@
+package projetoBloco1;
+
+public class ClienteFisico extends Pessoa {
+
+	public ClienteFisico(String nome, String telefone, String endereco) {
+		String cpf;
+		
+		public void solicitarDesbloqueio() {
+			Gerente.alterarStatus();
+		}
+		
+	}
+}
