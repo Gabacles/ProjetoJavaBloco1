1292c386362743087dcbe3177481b92556e339d9
MarjoryMatos <marjory_matos@hotmail.com>
2022-04-11 17:13:30 -0300

    conta corrente

--- src/conta/ContaCorrente.java
+++ src/conta/ContaCorrente.java
@@ -4,7 +4,7 @@ import pessoa.Pessoa;
 
 public class ContaCorrente extends Conta{
 	
-	private double taxa = 1.5;
+	private double taxa = 5;
 	
 	public ContaCorrente(Pessoa cliente, double saldo, String senha) {
 		super(cliente, saldo, senha);
@@ -18,4 +18,27 @@ public class ContaCorrente extends Conta{
 		this.taxa = taxa;
 	}
 
+	@Override
+	public void sacar(double valor, String senha) {
+		
+		if (valor <= saldo & this.getSenha().equals(senha)) {
+			saldo -= (valor + taxa);
+			tentativasErradas = 0;
+			extrato.add("+++++++++++++++++ " + new Date() + " +++++++++++++++++\n");
+			extrato.add("Saque de " + valor + " R$." +  "\n\t\t\t\t\t\tSaldo: " + saldo + " R$ \n");
+			System.out.println("Seu saque foi realizado com sucesso.");
+		} else if (valor > saldo) {
+			System.out.println("Seu saldo é insuficiente");
+		} else {
+			System.out.println("Sua senha está incorreta. Você pode redefini-la, caso precise. "
+					+ "Três tentativas erradas seguidas bloqueiam a conta.");		
+			if(tentativasErradas == 3) {
+				status = false;
+			} else {
+				tentativasErradas++;
+				System.out.println("Você ainda tem " + (3 - tentativasErradas) + " tentativas restantes.");
+			}
+		}	
+
+	}
 }
